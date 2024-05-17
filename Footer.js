40d06eb99d945d0cdb88b64955ebaeb8811bd181
Thawn9703 <thangndse171886@fpt.edu.vn>
2024-05-17 22:02:29 +0700

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css'; 
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-section">
+        <span>Version: 1.0.1</span>
+        <span>Last update: 10/5/2024</span>
+      </div>
+      <span>Make by: Unknown</span>
+    </footer>
+  );
+}
+
+export default Footer;
+
